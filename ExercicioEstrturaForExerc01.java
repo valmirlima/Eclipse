5734f4bcd1d3ebae8afb07140ec128ea6da779ab
valmirlima <44484796+valmirlima@users.noreply.github.com>
2018-11-02 19:30:01 -0300

    adicionei arquivos via upload

---
+++ ExercicioEstrturaForExerc01.java
@@ -0,0 +1,25 @@
+package course;
+
+import java.util.Scanner;
+
+public class ExercicioEstrturaForExerc01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int vt[] = null;
+		
+		System.out.println("Informe um numero inteiro!");
+		int n = sc.nextInt();
+		int res=0;
+		for (int i=1; i<=n; i++) {		
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
